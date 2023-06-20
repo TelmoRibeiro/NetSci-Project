d68eddb6afac300db3c5c002ee3ba80cabbaab3d
Telmo André Azevedo Ribeiro <telmoribeiro2706@gmail.com>
2023-06-20 14:21:22 +0100

    Add files via upload

--- edges/network.py
+++ edges/network.py
@@ -1,18 +1,17 @@
 def load_files():
     from sys import argv as console_arguments
-    if len(console_arguments) != 1 and len(console_arguments) != 5:
+    if len(console_arguments) != 1 and len(console_arguments) != 4:
         raise Exception("number of arguments not expected")
     if len(console_arguments) == 1:
         contacts_file   = "contacts.csv"
         meetings_file   = "meetings.csv"
         phonecalls_file = "phone_calls.csv"
-        relations_file  = "relationships.csv" 
-        return contacts_file, meetings_file, phonecalls_file, relations_file
+        return contacts_file, meetings_file, phonecalls_file
     contacts_file   = console_arguments[1]
     meetings_file   = console_arguments[2]
     phonecalls_file = console_arguments[3]
     relations_file  = console_arguments[4]
-    return contacts_file, meetings_file, phonecalls_file, relations_file
+    return contacts_file, meetings_file, phonecalls_file
 
 def read_file(filename):
     file = open(filename, 'r')
@@ -51,12 +50,11 @@ def print_network(network, filename=None):
     return
 
 def main():
-    contacts_file, meetings_file, phonecalls_file, relations_file = load_files()
+    contacts_file, meetings_file, phonecalls_file = load_files()
     contacts_network   = build_network(contacts_file)
     meetings_network   = build_network(meetings_file)
     phonecalls_network = build_network(phonecalls_file)
-    relations_network  = build_network(relations_file)
-    final_network = merge_networks([contacts_network, meetings_network, phonecalls_network, relations_network])
+    final_network = merge_networks([contacts_network, meetings_network, phonecalls_network])
     print_network(final_network, "network.csv")
     return
 
